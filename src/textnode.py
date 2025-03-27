b52f8114afcee2800b1395e34cad1ec57953efac
Coryf65 <Coryf65@users.noreply.github.com>
2025-03-27 17:42:21 -0500

    added a docstring fot TextNodes

--- src/textnode.py
+++ src/textnode.py
@@ -9,12 +9,18 @@ class TextType(Enum):
     IMAGES = "image"
     
 class TextNode:
+    """
+    class TextNode
+    
+    A class to represent an Text Node    
+    
+    :param text: The text content of the node
+    :param text_type: The type of text this node contains, which is a member of the TextType enum.
+    :param url: The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
+    """
     def __init__(self, text, text_type, url = None):
-        # The text content of the node
         self.text = text
-        # The type of text this node contains, which is a member of the TextType enum.
         self.text_type = text_type
-        # The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
         self.url = url
         
     def __eq__(self, other_text_node):
