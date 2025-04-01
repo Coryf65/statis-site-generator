679c497899e79fac7b8c51d5d855e5714f8e61a7
Coryf65 <Coryf65@users.noreply.github.com>
2025-03-31 21:52:17 -0500

    created and added split_nodes_delimiter along with the tests needed

--- src/utilities.py
+++ src/utilities.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 
-from textnode import TextType
+from textnode import TextNode, TextType
 
-class Utilities:
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    """
+    Splits the nodes based on the provided delimiter and returns a list of nodes.
     
-    #we need to be able to create TextNodes from raw markdown strings
-    
-    # given: This is text with a **bolded phrase** in the middle
-    # should be: [ TextNode("This is text with a ", TextType.TEXT), TextNode("bolded phrase", TextType.BOLD), TextNode(" in the middle", TextType.TEXT), ]
-    
-    def split_nodes_delimiter(old_nodes, delimiter, text_type):
-        """
-        Splits the nodes based on the provided delimiter and returns a list of nodes.
-        
-        :param old_nodes: The nodes to be split.
-        :param delimiter: The delimiter used for splitting the nodes ('`', '**').
-        :param text_type: The type of text (e.g., 'text', 'bold', 'link') for processing.
-        :return: a new list of nodes, where any "text" type nodes in the input list are (potentially) split into multiple nodes based on the syntax
-        """
-        if text_type == TextType.TEXT:
-            return old_nodes.split(delimiter)
-        elif text_type == TextType.LINK:
-            pass
-        elif text_type == TextType.CODE:
-            pass
-        else:
-            raise ValueError("Unsupported text type")
+    :param old_nodes: The nodes to be split.
+    :param delimiter: The delimiter used for splitting the nodes ('`', '**').
+    :param text_type: The type of text (e.g., 'text', 'bold', 'link') for processing.
+    :return: a new list of nodes, where any "text" type nodes in the input list are (potentially) split into multiple nodes based on the syntax
+    """
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
