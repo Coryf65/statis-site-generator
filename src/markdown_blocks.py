960ef58d280e6cc9b259f591f9517131ae08d6da
Coryf65 <Coryf65@users.noreply.github.com>
2025-04-01 21:50:28 -0500

    Add BlockType enum and block_to_block_type function with tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python3
 
+from enum import Enum
+
+
+class BlockType(Enum):
+    """
+    An enum to represent the different types of blocks in a Markdown document.
+    """
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
+
+
+def block_to_block_type(block):
+    """
+    takes a single block of markdown text as input and returns the BlockType representing the type of block it is
+    :param block: A single block of markdown text
+    :return: The BlockType representing the type of block
+    """
+    if block.startswith("# "):
+        return BlockType.heading
+    elif block.startswith("`"):
+        return BlockType.code
+    elif block.startswith("> "):
+        return BlockType.quote
+    elif block.startswith("- "):
+        return BlockType.unordered_list
+    elif block[0].isdigit() and block[1] == ".":
+        return BlockType.ordered_list
+    else:
+        return BlockType.paragraph  
+
 
 def markdown_to_blocks(markdown):
     """
